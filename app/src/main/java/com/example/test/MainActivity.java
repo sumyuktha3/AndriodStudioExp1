e6b1fdb47d6efb1ce96d596af4ccb04fc8672f2c
sumyuktha3 <75235818+sumyuktha3@users.noreply.github.com>
2022-04-26 09:25:13 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.test;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity { @Override
+protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); setContentView(R.layout.activity_main);
+    Toast toast=Toast.makeText(getApplicationContext(),"OnCreate Invoked",Toast.LENGTH_LONG);
+    toast.show();
+}
+
+    protected void onStart(){ super.onStart();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnStart Invoked",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onResume(){ super.onResume();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnResume Invoked",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onPause(){ super.onPause();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnPause Invoked",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onStop(){ super.onStop();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnStop Invoked",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onRestart(){ super.onRestart();
+
+        Toast toast=Toast.makeText(getApplicationContext(),"OnRestart Invoked",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onDestroy(){ super.onDestroy();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnDestroy Invoked",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+}
